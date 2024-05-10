89e858c2c6d34cb7d38270911b96415d55ee47b6
RuslanMakhanov <misterruslan1701@gmail.com>
2024-05-10 16:43:50 -0400

    Solved the first task from LeetCode (Two Sum)

---
+++ task1 (Two Sum)/TwoSum/Solution.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            int cur = nums[i];
+            int x = target - cur;
+            if (map.ContainsKey(x)) {
+                return new int[] { map[x], i };
+            }
+            map[cur] = i;
+        }
+        return null;
+    }
+}
